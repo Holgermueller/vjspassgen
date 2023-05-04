74d3b2b078286b8fd01914ce75052cca1763960f
holgermueller <9jholger@gmail.com>
2023-05-04 14:03:16 -0500

    added radio inputs to prepare for other functionality

--- scripts.js
+++ scripts.js
@@ -3,13 +3,19 @@
 const buttonTrigger = document.getElementById("generatePassword");
 
 const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
+
+let chars = alpha;
 
 function generatePassword() {
   let alphaLength = alpha.length;
   let newPassword = "";
+  chars += numbers;
+  chars += symbols;
 
   for (let i = 0; i < 8; i++) {
-    newPassword += alpha.charAt(Math.floor(Math.random() * alphaLength));
+    newPassword += chars.charAt(Math.floor(Math.random() * alphaLength));
 
     document.getElementById("passwordDisplay").innerHTML = newPassword;
   }
