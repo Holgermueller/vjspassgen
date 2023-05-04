db8801acd597aeeac6c3f1c4c9bce8853858a04b
holgermueller <9jholger@gmail.com>
2023-05-04 17:44:00 -0500

    changed radio to checkboxes

--- scripts.js
+++ scripts.js
@@ -9,10 +9,16 @@ const symbols = "!@#$%^&*_-+=";
 let chars = alpha;
 
 function generatePassword() {
+  let hasNum = document.getElementById("hasNumbers").value;
+  let hasChars = document.getElementById("hasChars").value;
   let alphaLength = alpha.length;
   let newPassword = "";
-  chars += numbers;
-  chars += symbols;
+
+  if (hasNum.checked) {
+    console.log(true);
+  }
+
+  console.log(hasNum.checked, hasChars);
 
   for (let i = 0; i < 8; i++) {
     newPassword += chars.charAt(Math.floor(Math.random() * alphaLength));
